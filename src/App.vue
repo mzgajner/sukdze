67318253478929a3a1bbf4acd84734d50d480dbd
mzgajner <mato@zgajner.com>
2025-01-02 21:57:05 +0100

    Extract `Navigation` to standalone component

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,5 @@
 <script lang="ts" setup>
-import { useRoute } from 'vue-router'
-
-const route = useRoute()
-const items = [
-  {
-    label: 'Edit',
-    icon: 'i-ant-design:edit-twotone',
-    to: '/edit',
-  },
-  {
-    label: 'Study',
-    icon: 'i-ant-design:thunderbolt-twotone',
-    to: '/study',
-  },
-  {
-    label: 'Settings',
-    icon: 'i-ant-design:setting-twotone',
-    to: '/settings',
-  },
-]
+import Navigation from '#components/Navigation.vue'
 </script>
 
 <template>
@@ -28,18 +9,7 @@ const items = [
       <div class="p-4 flex-1 overflow-y-scroll">
         <RouterView />
       </div>
-      <UNavigationMenu
-        v-if="!route.meta.hideNav"
-        :items="items"
-        variant="link"
-        :ui="{
-          link: 'text-base justify-center',
-          linkLeadingIcon: 'size-6',
-          list: 'justify-between',
-          root: 'w-full [&>div]:min-w-full px-2 bg-[var(--ui-bg-elevated)]',
-          item: 'w-1/3',
-        }"
-      />
+      <Navigation />
     </main>
   </UApp>
 </template>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,37 @@
+<script lang="ts" setup>
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+const items = [
+  {
+    label: 'Edit',
+    icon: 'i-ant-design:edit-twotone',
+    to: '/edit',
+  },
+  {
+    label: 'Study',
+    icon: 'i-ant-design:thunderbolt-twotone',
+    to: '/study',
+  },
+  {
+    label: 'Settings',
+    icon: 'i-ant-design:setting-twotone',
+    to: '/settings',
+  },
+]
+</script>
+
+<template>
+  <UNavigationMenu
+    v-if="!route.meta.hideNav"
+    :items="items"
+    variant="link"
+    :ui="{
+      link: 'text-base justify-center',
+      linkLeadingIcon: 'size-6',
+      list: 'justify-between',
+      root: 'w-full [&>div]:min-w-full px-2 bg-[var(--ui-bg-elevated)]',
+      item: 'w-1/3',
+    }"
+  />
+</template>
