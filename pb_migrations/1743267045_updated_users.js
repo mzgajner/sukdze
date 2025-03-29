eed225cd2e2cb9c1f123c5a1d6727e36db81a807
mzgajner <mato@zgajner.com>
2025-03-29 17:51:08 +0100

    Try random migration to see if it works in prod

---
+++ pb_migrations/1743267045_updated_users.js
@@ -0,0 +1,27 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("_pb_users_auth_")
+
+  // add field
+  collection.fields.addAt(7, new Field({
+    "hidden": true,
+    "id": "number2704281778",
+    "max": null,
+    "min": null,
+    "name": "age",
+    "onlyInt": false,
+    "presentable": false,
+    "required": false,
+    "system": false,
+    "type": "number"
+  }))
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("_pb_users_auth_")
+
+  // remove field
+  collection.fields.removeById("number2704281778")
+
+  return app.save(collection)
+})
